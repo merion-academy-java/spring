f2f7a3e3efeef9af91ced10ca90e03ef2c442ed5
pocteg <pocteg@gmail.com>
2023-06-10 19:12:55 +0300

    Add create request

--- src/main/java/com/merion/spring/book/controller/BookApiController.java
+++ src/main/java/com/merion/spring/book/controller/BookApiController.java
@@ -3,9 +3,7 @@ package com.merion.spring.book.controller;
 import com.merion.spring.base.exception.ResourceNotFoundException;
 import com.merion.spring.book.entity.BookEntity;
 import com.merion.spring.book.service.BookService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -30,5 +28,10 @@ public class BookApiController {
     @GetMapping("/api/v1/book/{id}")
     public BookEntity byId(@PathVariable Integer id){
         return bookService.byId(id).orElseThrow(ResourceNotFoundException::new);
+    }
+
+    @PostMapping("/api/v1/book")
+    public BookEntity create(@RequestBody BookEntity request){
+        return bookService.create(request.getTitle(), request.getDescription());
     }
 }
--- src/main/java/com/merion/spring/book/service/BookService.java
+++ src/main/java/com/merion/spring/book/service/BookService.java
@@ -21,17 +21,26 @@ public class BookService {
         for (int i = 0; i < 100; i++) {
             BookEntity book = new BookEntity();
             book.setId(i);
-            book.setTitle("Book #"+random.nextInt(100, 999));
+            book.setTitle("Book #" + random.nextInt(100, 999));
             book.setDescription("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.");
             bookStorage.add(book);
         }
     }
 
-    public List<BookEntity> all(){
+    public List<BookEntity> all() {
         return bookStorage;
     }
 
-    public Optional<BookEntity> byId( Integer id){
-        return bookStorage.stream().filter( (book -> book.getId().equals(id))).findFirst();
+    public Optional<BookEntity> byId(Integer id) {
+        return bookStorage.stream().filter((book -> book.getId().equals(id))).findFirst();
+    }
+
+    public BookEntity create(String title, String description) {
+        BookEntity book = new BookEntity();
+        book.setId(bookStorage.size());
+        book.setTitle(title);
+        book.setDescription(description);
+        bookStorage.add(book);
+        return book;
     }
 }
