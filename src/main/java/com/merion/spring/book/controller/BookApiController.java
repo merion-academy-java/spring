1b27e0868abb439d5cf699993ead8ed5434a1cc4
pocteg <pocteg@gmail.com>
2023-06-10 19:07:48 +0300

    Get request

--- src/main/java/com/merion/spring/book/controller/BookApiController.java
+++ src/main/java/com/merion/spring/book/controller/BookApiController.java
@@ -1,14 +1,34 @@
 package com.merion.spring.book.controller;
 
+import com.merion.spring.base.exception.ResourceNotFoundException;
+import com.merion.spring.book.entity.BookEntity;
+import com.merion.spring.book.service.BookService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class BookApiController {
+    private final BookService bookService;
+
+    public BookApiController(BookService bookService) {
+        this.bookService = bookService;
+    }
 
     @GetMapping("/")
     public String ok(){
         return "ok";
     }
 
+    @GetMapping("/api/v1/book")
+    public List<BookEntity> all(){
+        return bookService.all();
+    }
+
+    @GetMapping("/api/v1/book/{id}")
+    public BookEntity byId(@PathVariable Integer id){
+        return bookService.byId(id).orElseThrow(ResourceNotFoundException::new);
+    }
 }
